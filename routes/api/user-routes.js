666da703167925dd4809f57b49605ba94f9dd8ca
Jake <xxjakexx14@gmail.com>
2021-11-15 14:20:34 -0500

    added functions for implementing user post

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,31 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+
+    // Verify user
+  });
+});
+
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -58,8 +83,8 @@ router.put("/:id", (req, res) => {
   User.update(req.body, {
     individualHooks: true,
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
     .then((dbUserData) => {
       if (!dbUserData[0]) {
